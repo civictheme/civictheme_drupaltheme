624b0cfef76dd628131b6fc7d918d2626bc256c7
Deployer Robot <deployer@salsadigital.com.au>
2021-11-17 19:48:26 +1100

    Release 0.21.2.
    Dsisabled debug.
    Added creation of user accounts and fixed site email. (#3)

--- civic-library/components/00-base/fonts/fonts.stories.js
+++ civic-library/components/00-base/fonts/fonts.stories.js
@@ -11,12 +11,29 @@ export const Fonts = () => {
     ...SCSS_VARIABLES['civic-fonts'],
   ])];
 
+  const types = {
+    Regular: 'regular',
+    Italic: 'italic',
+    'Thin 300': 'thin-300',
+    Bold: 'bold',
+    'Bold 700': 'bold-700',
+    'Bold Italic': 'bold-italic',
+  };
+
   let html = '';
 
   for (const i in Object.values(fonts)) {
     html += `<div class="example-container">`;
+
     html += `<div class="example-container__title">${fonts[i]}</div>`;
-    html += `<div class="example-container__content story-font--${fonts[i]}">The quick brown fox jumps over the lazy dog</div>`;
+
+    html += `<div class="example-container__content">`;
+    for (const name in types) {
+      html += `<div class="example-container__subtitle">${name}</div>`;
+      html += `<div class="example-container__subcontent story-font--${types[name]}--${fonts[i]}">The quick brown fox jumps over the lazy dog</div>`;
+    }
+    html += `</div>`;
+
     html += `</div>`;
   }
 
